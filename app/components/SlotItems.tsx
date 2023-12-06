1522c4f837e98a36531744fc4a3d63e4741af1d1
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-12-06 07:13:20 +0100

    plan queue

--- app/components/SlotItems.tsx
+++ app/components/SlotItems.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { useRouter } from "next/navigation";
+// import { IoCalendarNumberOutline } from "react-icons/io5";
 import React from "react";
 
 interface SlotItemsProps {
   date: string;
   slot: string;
   href: string;
+  name: string;
 }
 const SlotItems: React.FC<SlotItemsProps> = ({ date, slot, href }) => {
   const router = useRouter();
@@ -16,10 +18,15 @@ const SlotItems: React.FC<SlotItemsProps> = ({ date, slot, href }) => {
     router.push(href);
   };
   return (
-    <button className="relative group flex items-center overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+    <button
+      onClick={onClick}
+      className="relative group flex items-center overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
+    >
       <div className="relative min-h-[64px] m-w-[64px]">
+        {/* <IoCalendarNumberOutline /> */}
         <p>Current Date: {formattedDate}</p>
       </div>
+      <p className="font-medium py-5">{slot}</p>
     </button>
   );
 };
