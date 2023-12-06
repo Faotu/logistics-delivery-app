d844c9aad4e1b19697bbfdb57712bcb6000397c7
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-12-06 14:15:43 +0100

    database connection

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,7 @@ import SlotItems from "../components/SlotItems";
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
         <MainHeader>
           <div className="mb-2 ">
             <h1 className="text-white text-3xl font-semibold">
@@ -15,7 +15,18 @@ export default function Home() {
             </div>
           </div>
         </MainHeader>
-      </h1>
+      </div>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1
+            className="text-white text-2xl font-semibold 
+          "
+          >
+            Planner Queue
+          </h1>
+        </div>
+        {/* <PageContent songs={songs} /> */}
+      </div>
     </div>
   );
 }
