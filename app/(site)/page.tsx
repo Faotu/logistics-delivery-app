419a0449546884299fef45d993d9dd5bf2357055
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-12-06 06:19:34 +0100

    slot current date

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
               Welcome to delivery planner
             </h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4 ">
-              <SlotItems />
+              <SlotItems date="date" />
             </div>
           </div>
         </MainHeader>
--- app/components/SlotItems.tsx
+++ app/components/SlotItems.tsx
@@ -1,7 +1,27 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-const SlotItems = () => {
-  return <div>Slot 1</div>;
+interface SlotItemsProps {
+  date: string;
+  slot: string;
+  href: string;
+}
+const SlotItems: React.FC<SlotItemsProps> = ({ date, slot, href }) => {
+  const router = useRouter();
+  const currentDate = new Date();
+  const formattedDate = currentDate.toDateString();
+  const onClick = () => {
+    // Implement Auth before push
+    router.push(href);
+  };
+  return (
+    <button className="relative group flex items-center overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+      <div className="relative min-h-[64px] m-w-[64px]">
+        <p>Current Date: {formattedDate}</p>
+      </div>
+    </button>
+  );
 };
 
 export default SlotItems;
