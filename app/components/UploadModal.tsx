d8eedc945671195cb57d02c932800099d4532e9f
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-12-13 15:00:44 +0100

    upload customer details

--- app/components/UploadModal.tsx
+++ app/components/UploadModal.tsx
@@ -1,14 +1,22 @@
 "use client";
 
 import React, { useState } from "react";
+import uniqid from "uniqid";
 import Modal from "./Modal";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 import useUploadModal from "@/hooks/useUploadModal";
 import Input from "./Input";
+import Button from "./Button";
+import toast from "react-hot-toast";
+import { useUser } from "@/hooks/useUser";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 const UploadModal = () => {
-  const [isLoading, setIsLoading] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   const uploadModal = useUploadModal();
+  const { user } = useUser();
+  const supabaseClient = useSupabaseClient();
+
   const { register, handleSubmit, reset } = useForm<FieldValues>({
     defaultValues: {
       Name: "",
@@ -17,7 +25,7 @@ const UploadModal = () => {
       drop_location: "",
     },
   });
-  const onChange = (opne: boolean) => {
+  const onChange = (open: boolean) => {
     if (!open) {
       reset();
       uploadModal.onClose();
@@ -26,6 +34,23 @@ const UploadModal = () => {
 
   const onSubmit: SubmitHandler<FieldValues> = async (values) => {
     // Upload customer details to supabase
+
+    try {
+      setIsLoading(true);
+
+      const imageFile = values.image?.[0];
+
+      if (!imageFile || !user) {
+        toast.error("missing fields");
+        return;
+      }
+
+      const uniqueID = uniqid();
+    } catch (error) {
+      toast.error("There was an error");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -35,13 +60,45 @@ const UploadModal = () => {
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
         <Input
-          id="title"
+          id="Customer name"
           disabled={isLoading}
-          {...register("title", { required: true })}
-          placeholder="Customer details"
+          {...register("Customer name", { required: true })}
+          placeholder="Customer Name"
         />
+        <Input
+          id="customer ID"
+          disabled={isLoading}
+          {...register("customer ID", { required: true })}
+          placeholder="Customer ID"
+        />
+        <Input
+          id="pick"
+          disabled={isLoading}
+          {...register("pick", { required: true })}
+          placeholder="Pickup Location"
+        />
+
+        <Input
+          id="drop"
+          disabled={isLoading}
+          {...register("drop", { required: true })}
+          placeholder="Drop Loaction"
+        />
+        <div>
+          <div className="pb-1">Select customer Image</div>
+          <Input
+            id="image"
+            type="file"
+            disabled={isLoading}
+            accept="image/*"
+            {...register("image", { required: true })}
+          />
+        </div>
+        <Button disabled={isLoading} type="submit">
+          Create
+        </Button>
       </form>
     </Modal>
   );
