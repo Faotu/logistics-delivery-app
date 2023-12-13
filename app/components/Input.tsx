29e18c157fff940dac6089858bf0cc032d3a816f
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-12-13 14:18:47 +0100

    input details

---
+++ app/components/Input.tsx
@@ -0,0 +1,13 @@
+import { forwardRef } from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, disabled, ...props }, ref) => {
+    return <input />;
+  }
+);
+
+Input.displayName = "Input";
+
+export default Input;
--- app/components/UploadModal.tsx
+++ app/components/UploadModal.tsx
@@ -1,15 +1,31 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Modal from "./Modal";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 import useUploadModal from "@/hooks/useUploadModal";
+import Input from "./Input";
 
 const UploadModal = () => {
+  const [isLoading, setIsLoading] = useState();
   const uploadModal = useUploadModal();
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      Name: "",
+      id: "",
+      pick_location: "",
+      drop_location: "",
+    },
+  });
   const onChange = (opne: boolean) => {
     if (!open) {
+      reset();
       uploadModal.onClose();
     }
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    // Upload customer details to supabase
   };
 
   return (
@@ -19,7 +35,14 @@ const UploadModal = () => {
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      Upload Modal
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          id="title"
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder="Customer details"
+        />
+      </form>
     </Modal>
   );
 };
